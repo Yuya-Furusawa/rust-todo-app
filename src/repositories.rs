bdc8f1ec60fdc5302f8d11ecc11c4e7fa05afe38
Yuya Furusawa <fullyou0798@gmail.com>
2022-10-20 16:18:28 +0900

    ファイルの分割

---
+++ src/repositories.rs
@@ -0,0 +1,89 @@
+use std::{
+  collections::HashMap,
+  sync::{Arc, RwLock},
+};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+// 起こりうるエラーの定義
+#[derive(Debug, Error)]
+enum RepositoryError {
+    #[error("NotFound id is {0}")]
+    NotFound(i32),
+}
+
+// トレイトで共通の振る舞い（CRUD）を定義する
+// これを継承した構造体はCRUDできるようになる
+pub trait TodoRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
+    fn create(&self, payload: CreateTodo) -> Todo;
+    fn find(&self, id: i32) -> Option<Todo>;
+    fn all(&self) -> Vec<Todo>;
+    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo>;
+    fn delete(&self, id: i32) -> anyhow::Result<Todo>;
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct Todo {
+    id: i32,
+    text: String,
+    completed: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct CreateTodo {
+    text: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct UpdateTodo {
+    text: Option<String>,
+    completed: bool,
+}
+
+impl Todo {
+    pub fn new(id: i32, text: String) -> Self {
+        Self {
+            id,
+            text,
+            completed: false,
+        }
+    }
+}
+
+// とりあえずTODOデータをHashMapに保存する
+type TodoDatas = HashMap<i32, Todo>;
+
+#[derive(Debug, Clone)]
+pub struct TodoRepositoryForMemory {
+    store: Arc<RwLock<TodoDatas>>,
+}
+
+impl TodoRepositoryForMemory {
+    pub fn new() -> Self {
+        TodoRepositoryForMemory {
+            store: Arc::default(),
+        }
+    }
+}
+
+impl TodoRepository for TodoRepositoryForMemory {
+    fn create(&self, payload: CreateTodo) -> Todo {
+        todo!()
+    }
+
+    fn find(&self, id: i32) -> Option<Todo> {
+        todo!()
+    }
+
+    fn all(&self) -> Vec<Todo> {
+        todo!()
+    }
+
+    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
+        todo!()
+    }
+
+    fn delete(&self, id: i32) -> anyhow::Result<Todo> {
+        todo!()
+    }
+}
