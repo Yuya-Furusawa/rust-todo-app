4c9c0809de49cdb845339e12a19af4c01b8c3093
Yuya Furusawa <fullyou0798@gmail.com>
2022-10-31 16:14:22 +0900

    簡素なフロント作り

--- todo-front/src/App.tsx
+++ todo-front/src/App.tsx
@@ -1,10 +1,94 @@
-import { useState } from 'react'
+import { FC, useState } from "react"
+import 'modern-css-reset'
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import { Box, Stack, Typography } from '@mui/material'
+import { NewTodoPayload, Todo } from "./types/todos"
+import TodoForm from './components/TodoForm'
+import TodoList from "./components/TodoList"
 
-function App() {
+const TodoApp: FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const createId = () => todos.length + 1
+
+  const onSubmit = (payload: NewTodoPayload) => {
+    if (!payload.text) return
+    setTodos((prev) => [
+      {
+        id: createId(),
+        text: payload.text,
+        completed: false,
+      },
+      ...prev,
+    ])
+  }
+
+  const onUpdate = (updateTodo: Todo) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === updateTodo.id) {
+          return {
+            ...todo,
+            ...updateTodo,
+          }
+        }
+        return todo
+      })
+    )
+  }
+
   return (
-    <div>
-      Todo App
-    </div>
+    <>
+      <Box
+        sx={{
+          backgroundColor: 'white',
+          borderBottom: '1px solid gray',
+          display: 'flex',
+          alignItems: 'center',
+          position: 'fixed',
+          top: 0,
+          p: 2,
+          width: '100%',
+          height: 80,
+          zIndex: 3,
+        }}
+      >
+        <Typography variant="h1">Todo App</Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          p: 5,
+          mt: 10,
+        }}
+      >
+        <Box maxWidth={700} width="100%">
+          <Stack spacing={5}>
+            <TodoForm onSubmit={onSubmit}/>
+            <TodoList todos={todos} onUpdate={onUpdate} />
+          </Stack>
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: 30,
+    },
+    h2: {
+      fontSize: 20,
+    }
+  }
+})
+
+const App: FC = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <TodoApp />
+    </ThemeProvider>
   )
 }
 
